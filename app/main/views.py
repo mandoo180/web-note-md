fb868c7911f1db569a49f59dc6f33c0edcf2432b
최경수 <mandoo180@gmail.com>
2025-06-13 16:15:21 +0900

    add markdown-it-py

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,24 @@
 import os
 
 from flask import render_template
-from markdown import markdown
+from markdown_it import MarkdownIt
+from mdit_py_plugins.footnote import footnote_plugin
+from mdit_py_plugins.tasklists import tasklists_plugin
+# from pygments.formatters import HtmlFormatter
 from app.main import main
 
+md = MarkdownIt("commonmark", {
+    "html": True,
+    "linkify": True
+}).use(footnote_plugin).use(tasklists_plugin)
+
+# css = HtmlFormatter().get_style_defs(".highlight")
 
 @main.route('/')
 def index():
     md_file = f"{os.environ['WEB_NOTE_HOME']}/note1.md"
     with open(md_file, "r", encoding="utf-8") as f:
         md_text = f.read()
-    md_out = markdown(md_text)
+    md_out = md.render(md_text)
 
     return render_template('index.html', md_out=md_out)
