d0f6299710e05b674959d419a8a925ae69624c97
Kyeongsoo Choi <mandoo180@gmail.com>
2025-06-15 04:47:32 +0900

    add more templates

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
 import os
+import re
 
+from pathlib import Path
 from flask import render_template
 from markdown_it import MarkdownIt
 from mdit_py_plugins.footnote import footnote_plugin
@@ -16,9 +18,20 @@ md = MarkdownIt("commonmark", {
 
 @main.route('/')
 def index():
-    md_file = f"{os.environ['WEB_NOTE_HOME']}/note1.md"
+    return render_template('index.html')
+
+
+@main.route('/list')
+def file_list():
+    md_files = list(Path(f"{os.environ['WEB_NOTE_HOME']}").glob("*.md"))
+    return render_template('list.html', md_files=md_files)
+
+
+@main.route('/view/<filename>')
+def view(filename):
+    md_file = f"{os.environ['WEB_NOTE_HOME']}/{filename}"
     with open(md_file, "r", encoding="utf-8") as f:
-        md_text = f.read()
+        md_text = re.sub(r"(?s)\A\+\+\+.*?\+\+\+\n*", "", f.read())
     md_out = md.render(md_text)
 
-    return render_template('index.html', md_out=md_out)
+    return render_template('view.html', md_out=md_out)
