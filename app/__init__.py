4c829ba2a6bc70eb5db3db855bf2a6655b8422ef
최경수 <mandoo180@gmail.com>
2025-06-16 19:05:14 +0900

    add auth function

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_login import LoginManager
 
+
+login_manager = LoginManager()
+login_manager.login_view = 'main.login'
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = 'not-safe-key'
+
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
 
--- app/main/views.py
+++ app/main/views.py
@@ -2,11 +2,13 @@ import os
 import re
 
 from pathlib import Path
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from flask_login import current_user, UserMixin, login_user
 from markdown_it import MarkdownIt
 from mdit_py_plugins.footnote import footnote_plugin
 from mdit_py_plugins.tasklists import tasklists_plugin
 # from pygments.formatters import HtmlFormatter
+from app import login_manager
 from app.main import main
 
 md = MarkdownIt("commonmark", {
@@ -15,6 +17,17 @@ md = MarkdownIt("commonmark", {
 }).use(footnote_plugin).use(tasklists_plugin)
 
 # css = HtmlFormatter().get_style_defs(".highlight")
+
+class User(UserMixin):
+    def __init__(self, username):
+        self.id = username
+        self.username = username
+
+
+@login_manager.user_loader
+def load_user(username):
+    return User(username)
+
 
 @main.route('/')
 def index():
@@ -30,3 +43,16 @@ def view(filename):
     md_out = md.render(md_text)
 
     return render_template('view.html', md_out=md_out)
+
+
+@main.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    if request.method == 'POST':
+        username = request.form['username']
+        # password = request.form['password']
+        user = User(username)
+        login_user(user)
+        return redirect(url_for('main.index'))
+    return render_template('login.html')
